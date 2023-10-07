244a73b8ea62fd1a48d3670efbc658730407a77b
VoidPep <pedrohm1009@gmail.com>
2023-10-07 18:28:58 -0300

    map.js configurado

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,20 +1,18 @@
-const cssLink = document.createElement("link");
-cssLink.rel = "stylesheet";
-cssLink.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
-cssLink.integrity = "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC";
-cssLink.crossOrigin = "anonymous";
-document.head.appendChild(cssLink);
+document.addEventListener("DOMContentLoaded", function () {
+    const bootstrap = document.createElement("link");
+    bootstrap.rel = "stylesheet";
+    bootstrap.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css";
+    bootstrap.integrity = "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC";
+    bootstrap.crossOrigin = "anonymous";
 
-const leafletCss = document.createElement("link");
-leafletCss.rel = "stylesheet";
-leafletCss.href = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css";
-leafletCss.integrity = "sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY:";
-leafletCss.crossOrigin = "";
+    const appCss = document.createElement("link");
+    appCss.rel = "stylesheet";
+    appCss.href = "/src/css/app.css";
+    appCss.type = "text/css"
 
-document.head.appendChild(leafletCss);
 
-const leafletJs = document.createElement("script");
-leafletJs.src = "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js";
-leafletJs.integrity = "sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=";
-leafletJs.crossOrigin = "";
-document.head.appendChild(leafletJs);
+    document.head.appendChild(appCss);
+    document.head.appendChild(bootstrap);
+
+    console.log(document.head);
+});
